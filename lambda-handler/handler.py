228790eec391b74e2c2a3b825ccaa80ce4d8022d
sujatadhl <dahalsujata52@gmail.com>
2024-06-21 07:23:28 +0545

    Added lambda file

---
+++ lambda-handler/handler.py
@@ -0,0 +1,21 @@
+import json
+import boto3
+
+sns = boto3.client('sns')
+
+def handler(event, context):
+    for record in event['Records']:
+        msg = record['body']
+        sqs_message = json.loads(msg)
+        bucket = sqs_message['Records'][0]['s3']['bucket']['name']
+        object_key = sqs_message['Records'][0]['s3']['object']['key']
+        message = f"This object has been added: {object_key} in bucket: {bucket}"
+        
+        
+        response = sns.publish(
+            TopicArn='arn:aws:sns:us-east-1:426857564226:sns-topic-sujata',
+            Message=json.dumps({'default': message}),
+            MessageStructure='json'
+        )
+        print(f"Published to SNS: {response}")
+    return "Success"
